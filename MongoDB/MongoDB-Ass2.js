d1222ebc71da6ffb668aaeaca78f635c6fd0b17a
AshwinDavidJr <ashwindavid11@gmail.com>
2021-08-27 15:44:57 +0530

    commiting additional things

---
+++ MongoDB/MongoDB-Ass2.js
@@ -0,0 +1,16 @@
+/* to use mongoimport
+
+1.download mongodb-tools.zip
+2.copy the files in bin folder to the bin folder of mongosh (optional)
+3.open mongoshell and connect to your atlas
+4.open another command prompt and change directory to the folder with files mentioned in step 2.
+5. open atlas, select the cluster, choose 3 dots and select command line tools.
+6.copy paste the command for import in the command prompt(step 4). Make sure you change the required
+fiels with actual data.
+7.provide the path of your json file
+8 execute the command.
+
+*/
+
+
+
