eb3c38064b64514a3e24b818ab75078840038956
AshwinDavidJr <ashwindavid11@gmail.com>
2021-09-07 21:51:22 +0530

    Commiting assignments

---
+++ JAVA_Training/src/Ass10/A10_q3.java
@@ -0,0 +1,18 @@
+package Ass10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class A10_q3 {
+
+    public static void main(String[] args) {
+         String str = "A quick brown fox jumps over a lazy frog";
+
+        String[] arrlist = str.split(" ");
+        System.out.println(Arrays.toString(arrlist));
+
+        String[] str2=Arrays.stream(arrlist).toArray(size->new String[size]);
+
+        System.out.println(Arrays.toString(str2));
+    }
+}
